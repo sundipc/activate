9cfb107200019112cabbf23155c42c49e1545eb2
Sundip Chauhan <sundip@Sundips-MacBook-Pro-2.local>
2025-10-18 20:01:30 +0100

    Fix: resolve navbar alignment issue and update color scheme

---
+++ my-app/src/pages/AboutUs.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+
+// function AboutUs() {
+//   return (
+//     <div>AboutUs</div>
+//   )
+// }
+
+// export default AboutUs
+
+
+import React from 'react'
+
+function AboutUS() {
+    return(
+        <div>AboutUs</div>
+    )
+}
+
+export default AboutUs
